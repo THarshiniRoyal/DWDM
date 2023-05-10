346203357f443232489d5a5d10f3e838b08bdf3f
THarshiniRoyal <115178073+THarshiniRoyal@users.noreply.github.com>
2023-05-10 15:47:08 +0530

    Add files via upload

---
+++ decision tree.r
@@ -0,0 +1,7 @@
+library(rpart)
+data(iris)
+iris_tree <- rpart(Species ~ ., data = iris)
+plot(iris_tree)
+text(iris_tree, use.n = TRUE)
+predictions <- predict(iris_tree, newdata = iris)
+head(predictions, 10)
